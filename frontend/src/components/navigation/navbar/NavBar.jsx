919e2cd3bbbb459c62e9d05fe8b9592fa9203759
Brady <bradymitchelmore@gmail.com>
2024-05-28 13:25:03 -0230

    Minor bug fix

--- frontend/src/components/navigation/navbar/NavBar.jsx
+++ frontend/src/components/navigation/navbar/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({ logo, children }) => (
     <div className="navbar">
         <div className="navbar-contents">
             <Link to="/">
-                <img src={logo} alt="Cropbytes Logo" className="navbar-logo" />
+                <img src={logo} alt="BM Logo" className="navbar-logo" />
             </Link>
             <div className="navbar-items">{children}</div>
         </div>
