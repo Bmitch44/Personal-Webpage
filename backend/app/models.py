cd196cf21e17122d312702770efbda82f842dae2
Brady <bradymitchelmore@gmail.com>
2024-05-31 15:24:12 -0230

    Update README.md and models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,8 @@ class Project(SQLModel, table=True):
     id: int = Field(primary_key=True)
     name: str
     description: str
+    details: str
+    expanded: str
     tools: list[str]
     url: str
     
