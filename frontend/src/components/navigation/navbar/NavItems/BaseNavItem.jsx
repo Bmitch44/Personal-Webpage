acc0efd5d40b67f2e4ee102ba7af4faca68b04da
Brady <bradymitchelmore@gmail.com>
2024-05-30 22:27:31 -0230

    started making website responsive

--- frontend/src/components/navigation/navbar/NavItems/BaseNavItem.jsx
+++ frontend/src/components/navigation/navbar/NavItems/BaseNavItem.jsx
@@ -4,15 +4,15 @@ import Dropdown from '../dropdown/Dropdown';
 import './BaseNavItem.css';
 import ForwardArrow from './Forward.png';
 
-const BaseNavItem = ({ text, link, isActive, dropdown, dropdownItems, bold}) => {
+const BaseNavItem = ({ text, link, isActive, dropdown, dropdownItems, bold, onClick}) => {
     const [showDropdown, setShowDropdown] = useState(false);
     const activeClass = isActive ? (bold ? 'active-white' : 'active') : '';
     console.log(activeClass);
     return (
     <div className={`nav-item ${bold && 'bold'}`} onMouseEnter={() => setShowDropdown(true)} onMouseLeave={() => setShowDropdown(false)}>
-        <a href={link} className={`nav-text ${bold ? `bold-text` : ''} ${activeClass}`}>
-            {text}
-        </a>
+        <NavLink to={link} className={`nav-text ${bold ? `bold-text` : ''} ${activeClass}`} onClick={onClick}>
+                {text}
+        </NavLink>
         {dropdown && <img src={ForwardArrow} alt="Dropdown Arrow" className='dropdown-arrow' />}
         {dropdown && showDropdown && <Dropdown items={dropdownItems} />}
     </div>
